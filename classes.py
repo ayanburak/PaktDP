6c8cbb1cd56fa0a10838e703a984a7e6ed971667
ayanburak <burakayan3558@gmail.com>
2026-01-12 16:37:23 +0300

    DataLoader and DataConfig added

---
+++ classes.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+
+class DataLoader():
+    """
+    Dosya yolu, Pandas DataFrame veya Dictionary formatındaki verileri 
+    yüklemek ve yönetmek için kullanılan sınıf.
+    """
+    def __init__(self, input_data):
+        self.data = pd.DataFrame()
+        
+        # Eğer girdi zaten bir Pandas DataFrame ise
+        if isinstance(input_data, pd.DataFrame):
+            self.data = input_data
+            
+        # Eğer girdi bir Dictionary ise
+        elif isinstance(input_data, dict):
+            self.data = pd.DataFrame(input_data)
+            
+        # Eğer girdi bir metin (dosya yolu) ise
+        elif isinstance(input_data, str):
+            path_lower = input_data.lower()
+            if path_lower.endswith(".csv"):
+                self.data = pd.read_csv(input_data)
+            elif path_lower.endswith((".xlsx", ".xls")):
+                self.data = pd.read_excel(input_data)
+            elif path_lower.endswith(".json"):
+                self.data = pd.read_json(input_data)
+            else:
+                raise ValueError(f"Desteklenmeyen dosya formatı: {os.path.basename(input_data)}")
+        
+        else:
+            raise TypeError("Girdi tipi bir dosya yolu (str), dict veya DataFrame olmalıdır.")
+
+    def get_data(self):
+        return self.data
+    
+class DataConfig():
+    
+    def __init__(self, df = pd.DataFrame()):
+        self.df = df
+        
+    def get_basic_stats(self):
+        return {
+            "row_count": self.df.shape[0],
+            "column_count": self.df.shape[1],
+            "total_cells": self.df.size
+        }
+    
+    # Hangi kolonda kaç tane Null değer olduğunu sayıp dict olarak döndürür
+    def get_null_count(self):
+        return self.df.isnull().sum().to_dict()
+    
+    # Hangi kolonda kaç tane Sıfır(0) değeri olduğunu sayıp dict olarak döndürür
+    def get_zeros_count(self):
+        return (self.df == 0).sum().to_dict()
+    
+    # Hangi kolonda hangi değerlerin tekrar ettiğini Dict formatında gösteren fonksiyon
+    def get_duplicates(self):
+            duplicate_report = {}
+            
+            for column in self.df.columns:
+                # value_counts() her değerden kaç tane olduğunu sayar
+                counts = self.df[column].value_counts()
+                
+                # Sadece sayısı 1'den büyük olanları (yani tekrar edenleri) filtreliyoruz
+                duplicates = counts[counts > 1].to_dict()
+                
+                # Eğer o kolonda tekrar eden değer varsa rapora ekliyoruz
+                if duplicates:
+                    duplicate_report[column] = duplicates
+                    
+            return duplicate_report
+    
+    
